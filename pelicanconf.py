e4a7dbd84e49b9f40fbb7fe1d64e7da422c59b49
Ilya Zonov <izonov@gmail.com>
2014-03-18 13:36:46 +0400

    Add initial Pelican configuration

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ilya Zonov'
+SITENAME = u"Puzan's Pages"
+SITEURL = 'http://localhost:8000'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = u'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = None
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/puzan'),
+          ('github', 'http://github.com/puzan'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
